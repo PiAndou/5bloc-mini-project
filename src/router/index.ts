cf4afc76765f6f020ecdbf2a215f5b47b80f8921
PiAndou <38055472+PiAndou@users.noreply.github.com>
2022-02-07 02:19:42 +0100

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import Deploy from "../views/Deploy"
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
@@ -12,11 +13,12 @@ const routes: Array<RouteConfig> = [
   {
     path: "/deploy",
     name: "Deploy",
+    component: Deploy,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Deploy.vue"),
+    // component: () =>
+    // import(/* webpackChunkName: "about" */ "../views/Deploy.vue"),
   },
 ];
 
